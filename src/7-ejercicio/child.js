30700645ec39c4f551eaba7b6ddc830e40affbbc
susana <sulopezfeijoo@gmail.com>
2022-06-22 09:28:00 +0200

    parent-child-practica

--- src/7-ejercicio/child.js
+++ src/7-ejercicio/child.js
@@ -1,28 +1,46 @@
-import * as fs from "fs/promises";
-
 console.log("child started working");
 
-let semillas = [];
+process.on("message", (arg) => {
+  parOImpar(arg.seed);
+});
 
-process.on("seed", (arg) => {
-  let seed = arg.seed;
-  semillas.push(seed);
-  if (seed === 0) {
+function parOImpar(seed) {
+  let seedOriginal = " " + seed;
+  let seedTexto = "" + seed;
+  let seedOpera = seed;
+  const start = process.hrtime();
+
+  try {
+    if (!parseInt(seed)) {
+      throw new Error("No es un número válido ");
+    }
+    if (seedOpera <= 0) {
+      seedTexto = "error";
+    }
+
+    while (seedOpera > 1) {
+      if (isEven(seedOpera)) {
+        seedOpera = seedOpera / 2;
+        seedTexto += " " + seedOpera;
+      } else {
+        seedOpera = seedOpera * 3 + 1;
+        seedTexto += " " + seedOpera;
+      }
+    }
+    const end = process.hrtime();
+    const tiempoEjecucion = end[1] - start[1];
+    seedTexto += " , Nanosecond Start: " + start[1];
+    seedTexto += " , Nanoseconds End: " + end[1];
+    seedTexto += " , Total Nanoseconds : " + tiempoEjecucion;
+
+    process.send({ semillas: seedTexto, seedOriginal: seedOriginal });
+    process.exit(0);
+  } catch (error) {
+    console.log(error + ", Error Fatal!");
+    process.send({ msg: "Child Error", err: error, seed: seed });
     process.exit(1);
   }
-  while (seed > 1) {
-    if (isEven(seed)) {
-      seed = seed / 2;
-      semillas.push(seed);
-    } else {
-      seed = seed * 3 + 1;
-      semillas.push(seed);
-    }
-  }
-  process.send({ semillas: semillas.toString });
-  console.log("Seed 1");
-  process.exit(0);
-});
+}
 
 function isEven(seed) {
   return seed % 2 == 0;
--- src/7-ejercicio/index.js
+++ src/7-ejercicio/index.js
@@ -1,23 +1,27 @@
 import * as cp from "child_process";
 import * as fs from "fs/promises";
 
-const child = cp.fork("./src/7-ejercicio/child.js");
+const seeds = [0, 1, 2, 3, 5, 7, 11, 13, "wsdfs"];
+let child;
+const scriptFile = process.argv[1];
 
-child.send({ seed: 4 });
+seeds.forEach((seed) => {
+  child = cp.fork("./src/7.1-child-practice/child.js");
+  child.send({ seed });
+  child.on("message", (arg) => {
+    writeFile(arg.semillas, arg.seedOriginal, arg.err);
+    console.log(`Recibido error: ${arg.msg} de ${arg.seed} con error: ${arg.err}`);
+  });
+});
 
-child.on("close", (code) => console.log("child closed, code: " + code));
-
-child.send({ close: true });
-
-console.log("parent started working");
-
-function writeFile(semillas) {
-  fs.writeFile(`${seed}.txt`, semillas)
+async function writeFile(semillas, seedOriginal) {
+  await fs
+    .writeFile(`${scriptFile}.${seedOriginal}.txt`, semillas)
     .then(onWriteEnd)
-    .catch((err) => console.log(`Error ${err}`))
-    .finally(() => console.log("acabei"));
+    .catch((err) => console.log(`${err}`))
+    .finally(() => console.log("Finalizado"));
 }
 
 function onWriteEnd() {
-  console.log("Resultado OK");
+  console.log(`Resultado OK`);
 }
